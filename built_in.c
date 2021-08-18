468c6437cba8524fe6c8cca9d61a278fe6e6dd84
aloicerains <aloiceokoth98@gmail.com>
2021-08-18 22:01:19 +0300

    implements environment variable

---
+++ built_in.c
@@ -0,0 +1,14 @@
+/**
+ * _environ - function prints the environment variables
+ *
+ * Return: 1
+ */
+#include "main.h"
+int _environ(void)
+{
+	char **en = environ;
+
+	for (; *en; en++)
+		printf("%s\n", *en);
+	return (1);
+}
